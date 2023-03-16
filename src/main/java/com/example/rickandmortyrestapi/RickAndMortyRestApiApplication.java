0a54520f33a2a7e2d7a4d38ba0fbfe09058c549d
Astrid Ottmann <astrid.ottmann@gmail.com>
2023-03-16 11:37:53 +0100

    initial commit

---
+++ src/main/java/com/example/rickandmortyrestapi/RickAndMortyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rickandmortyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickAndMortyRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rickandmortyrestapi/RickAndMortyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rickandmortyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
